a61ff6e20c3d71cc88ab9e2edada0d072cec77f1
Ioni <sami.repo@live.com>
2024-08-27 18:09:22 +0300

    function test

--- src/heroes/hello.ts
+++ src/heroes/hello.ts
@@ -1,9 +1,17 @@
 import factions from '../../Data/gameResources/hota_base.fhmod/factions.fhdb.json' assert { type: 'json' };
 
-const factionNames = Object.keys(factions[0].records);
-console.log("faction names " + factionNames);
 
-factionNames.forEach(faction => {
-    let name = factions[0].records[faction].untranslatedName;
-    console.log('faction id ' + faction + ":::: " + name);
-})
+const getFactionNames = (): string[] => {
+    const factionNames = Object.keys(factions[0].records);
+    const returnValue: string[] = [];
+    
+    factionNames.forEach(faction => {
+        let name = factions[0].records[faction].untranslatedName;
+        returnValue.push(name);
+    })
+    
+    return returnValue;
+}
+
+
+console.log('Faction names are: ' + getFactionNames());
