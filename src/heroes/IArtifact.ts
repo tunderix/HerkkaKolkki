c9ede7d85a5057d89dffa1d5c538219a65ff4ad4
MossGnome <[Eetu.hailikari@gmail.com]>
2024-08-29 14:31:30 +0300

    Added function for parsing artifacts.

---
+++ src/heroes/IArtifact.ts
@@ -0,0 +1,21 @@
+/*
+"sod.artifact.battleMachine.catapult": {
+    "class": "special",
+    "cost": 1000,
+    "value": 0,
+    "guard": 0,
+    "untranslatedName": "Catapult",
+    "slot": "",
+    "bmUnit": "sod.unit.catapult"
+},*/
+
+export default interface IArtifact {
+    identifier: string;
+    class: string;
+    cost: number;
+    value: number;
+    guard: number;
+    untranslatedName: string;
+    slot: string;
+    bmUnit: string;
+}
---
+++ src/heroes/IAuthors.ts
@@ -0,0 +1,7 @@
+export default interface IAuthors {
+    authors: IAuthor[];
+}
+export interface IAuthor {
+    name: string;
+    nick: string;
+}
